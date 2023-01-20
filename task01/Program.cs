9c00b0f3a3a9ad424ca086c81e6411bbf57c3146
Matwey <linnov1703@gmail.com>
2023-01-20 16:15:59 +0300

    Домашнее задание к семинару 2

---
+++ task01/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = new Random().Next(0, 999);
+System.Console.WriteLine(number);
+
+int result = 0;
+
+if (number > 99)
+{
+    result = (number / 10) % 10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+}
---
+++ task03/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//             0 1 2 3 4 5 6 7
+int [] days = {1,2,3,4,5,6,7};
+
+
+
+Console.Write("Введите  номер дня недели:");
+int number = Int32.Parse(Console.ReadLine());
+//Console.WriteLine(number);
+
+if(days[number-1] > days[4])
+{
+    System.Console.WriteLine("Выходной");
+}
+else
+{
+    System.Console.WriteLine("Будни");
+} 
